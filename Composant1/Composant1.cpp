852f8594a50d8ab9269f7413af8c2e0a40febea6
lola2016 <loubna.lrj@gmail.com>
2016-01-22 16:45:06 +0100

    ajout de la composante3 et compoàsant4

--- Composant1/Composant1.cpp
+++ Composant1/Composant1.cpp
@@ -1,7 +1,8 @@
 
 #include "Composant1.h"
+#include "Composant3.h"
 #include "Composant1Version.h"
-
+/*
 // declaration des fonctions internes
 int addition_interne(int a1, int a2);
 
@@ -9,7 +10,7 @@ int addition_interne(int a1, int a2);
 
 int composant1(int p1, int p2)
 {
-	return addition_interne(p1,p2);
+	return composant3(p1,p2);
 }
 
 int addition_interne(int a1, int a2)
@@ -21,3 +22,4 @@ char * getComposant1Version()
 {
 	return "Composant 1 version " COMPOSANT_VERSION_STR;
 }
+*/
---
+++ composant3/Composant3.cpp
@@ -0,0 +1,23 @@
+
+#include "Composant3.h"
+#include "Composant3Version.h"
+
+// declaration des fonctions internes
+int addition_interne(int a1, int a2);
+
+
+
+int composant3(int p1, int p2)
+{
+	return addition_interne(p1,p2);
+}
+
+int addition_interne(int a1, int a2)
+{
+	return a1+a2;
+}
+
+char * getComposant3Version()
+{
+	return "Composant 3 version " COMPOSANT_VERSION_STR;
+}
