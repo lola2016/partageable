b672223d9d29bd098ecb237835a432ff44cc2953
lola2016 <loubna.lrj@gmail.com>
2016-01-22 16:49:49 +0100

    add composant4

---
+++ composant4/Composant4.cpp
@@ -0,0 +1,23 @@
+
+#include "Composant1.h"
+#include "Composant4Version.h"
+
+// declaration des fonctions internes
+int addition_interne(int a1, int a2);
+
+
+
+int composant1(int p1, int p2)
+{
+	return addition_interne(p1,p2);
+}
+
+int addition_interne(int a1, int a2)
+{
+	return a1+a2;
+}
+
+char * getComposant1Version()
+{
+	return "Composant 4 version " COMPOSANT_VERSION_STR;
+}
